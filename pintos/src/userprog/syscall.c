792088410529ce0365e694becff52a223021b79c
youngchan2 <chani227@naver.com>
2024-10-04 03:04:47 +0900

    address validation/open/filesize/read

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -6,11 +6,31 @@
 #include "threads/thread.h"
 #include "devices/shutdown.h"
 #include "userprog/process.h"
+#include "threads/synch.h"
+#include "filesys/filesys.h"
+#include "threads/vaddr.h"
+#include "userprog/pagedir.h"
+#include "filesys/file.h"
+#include "devices/input.h"
 
 static void syscall_handler(struct intr_frame *);
+static struct lock filesys_lock;
+
+static void check_valid_ptr(const void *ptr)
+{
+  if (!(is_user_vaddr(ptr) && ptr > (void *)0x08048000))
+  {
+    exit(-1);
+  }
+  if (!pagedir_get_page(thread_current()->pagedir, ptr))
+  {
+    exit(-1);
+  }
+}
 
 void syscall_init(void)
 {
+  lock_init(&filesys_lock);
   intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
@@ -18,7 +38,9 @@ static void
 syscall_handler(struct intr_frame *f UNUSED)
 {
   // printf ("system call!\n");
-  uint32_t syscall_num = f->eax;
+  uint32_t *p = f->esp;
+  check_valid_ptr((void *)p);
+  uint32_t syscall_num = *p;
 
   switch (syscall_num)
   {
@@ -92,22 +114,98 @@ int wait(pid_t pid)
 
 bool create(const char *file, unsigned initial_size)
 {
+  lock_acquire(&filesys_lock);
+  bool result = filesys_create(file, initial_size);
+  lock_release(&filesys_lock);
+
+  return result;
 }
 
 bool remove(const char *file)
 {
+  lock_acquire(&filesys_lock);
+  bool result = filesys_remove(file);
+  lock_release(&filesys_lock);
+
+  return result;
 }
 
 int open(const char *file)
 {
+  lock_acquire(&filesys_lock);
+  struct file *open_file = filesys_open(file);
+
+  // 2024-10-02 이어서
+  struct thread *cur_thread = thread_current();
+  int fd;
+  if (open_file != NULL)
+  {
+    for (fd = 2; fd < FDT_SIZE; fd++)
+    {
+      if (cur_thread->fdt[fd] == NULL)
+        break;
+    }
+    if (fd == FDT_SIZE)
+    {
+      fd = -1;
+    }
+    else
+    {
+      cur_thread->fdt[fd] = open_file;
+    }
+  }
+  else
+  {
+    fd = -1;
+  }
+  lock_release(&filesys_lock);
+
+  return fd;
 }
 
 int filesize(int fd)
 {
+  struct thread *cur_thread = thread_current();
+  struct file *open_file = cur_thread->fdt[fd];
+
+  if (fd < 2 || fd > FDT_SIZE || open_file == NULL)
+  {
+    exit(-1);
+  }
+  lock_acquire(&filesys_lock);
+  int l = file_length(open_file);
+  lock_release(&filesys_lock);
+
+  return l;
 }
 
 int read(int fd, void *buffer, unsigned size)
 {
+  if (fd < 0 || fd >= FDT_SIZE)
+  {
+    exit(-1);
+  }
+
+  struct thread *cur_thread = thread_current();
+  lock_acquire(&filesys_lock);
+  if (fd == 0)
+  {
+    // 2024-10-04 이어서
+  }
+  else if (fd > 2)
+  {
+    struct file *open_file = cur_thread->fdt(fd);
+    int actual_read = 0;
+    if (open_file == NULL)
+    {
+      lock_release(&filesys_lock);
+      exit(-1);
+    }
+    actual_read = file_read(open_file, buffer, size);
+    lock_release(&filesys_lock);
+
+    return actual_read;
+  }
 }
 
 int write(int fd, const void *buffer, unsigned size)
