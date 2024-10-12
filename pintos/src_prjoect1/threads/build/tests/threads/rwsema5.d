3480810acbd0f6dee6f7984d0aa81a6bdf19441f
youngchan2 <chani227@naver.com>
2024-10-13 00:17:41 +0900

    pass except pipe-short&multi-oom

---
+++ pintos/src_prjoect1/threads/build/tests/threads/rwsema5.d
@@ -0,0 +1,6 @@
+tests/threads/rwsema5.o: ../../tests/threads/rwsema5.c ../../lib/stdio.h \
+ ../../lib/debug.h ../../lib/stdarg.h ../../lib/stdbool.h \
+ ../../lib/stddef.h ../../lib/stdint.h ../../lib/kernel/stdio.h \
+ ../../tests/threads/tests.h ../../threads/init.h ../../threads/synch.h \
+ ../../lib/kernel/list.h ../../threads/thread.h ../../devices/timer.h \
+ ../../lib/round.h ../../threads/malloc.h
---
+++ pintos/src_prjoect1/threads/build/tests/threads/seqlock2.d
@@ -0,0 +1,7 @@
+tests/threads/seqlock2.o: ../../tests/threads/seqlock2.c \
+ ../../lib/stdio.h ../../lib/debug.h ../../lib/stdarg.h \
+ ../../lib/stdbool.h ../../lib/stddef.h ../../lib/stdint.h \
+ ../../lib/kernel/stdio.h ../../tests/threads/tests.h \
+ ../../threads/init.h ../../threads/synch.h ../../lib/kernel/list.h \
+ ../../threads/thread.h ../../devices/timer.h ../../lib/round.h \
+ ../../threads/malloc.h
